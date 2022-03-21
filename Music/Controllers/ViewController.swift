3f581a0970ae12f2f9c05756b5df057436677c55
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-22 00:32:41 +0300

    Initial Commit

---
+++ Music/Controllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Никита Коголенок on 22.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Variable
+    
+    // MARK: -
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+}
