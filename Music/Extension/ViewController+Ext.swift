96e914f23ea2ae3910d37b8d8a4301e8100cc3d4
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-22 01:02:53 +0300

    Present the Player in future

--- Music/Extension/ViewController+Ext.swift
+++ Music/Extension/ViewController+Ext.swift
@@ -21,5 +21,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let position = indexPath.row
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "Player") else { return }
+        present(vc, animated: true)
     }
 }
