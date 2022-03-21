6852d48d36f0006b49247ae8d933ed255ee05ec0
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-22 01:12:21 +0300

    Created model of Song and add song to the array of song

--- Music/Controllers/ViewController.swift
+++ Music/Controllers/ViewController.swift
@@ -20,11 +20,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         createDelegate()
+        configureSong()
     }
     
     // MARK: - Methods
     private func createDelegate() {
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    private func configureSong() {
+        songs.append(Song(name: "Background music", albomName: "123 Other", artistName: "Rnado", imageName: "cover1", trackName: "song1"))
+        songs.append(Song(name: "Havana", albomName: "Havana album", artistName: "Camilla Cabello", imageName: "cover2", trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida", albomName: "123 Something", artistName: "Coldplay", imageName: "cover3", trackName: "song3"))
+        songs.append(Song(name: "Background music", albomName: "123 Other", artistName: "Rnado", imageName: "cover1", trackName: "song1"))
+        songs.append(Song(name: "Havana", albomName: "Havana album", artistName: "Camilla Cabello", imageName: "cover2", trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida", albomName: "123 Something", artistName: "Coldplay", imageName: "cover3", trackName: "song3"))
+        songs.append(Song(name: "Background music", albomName: "123 Other", artistName: "Rnado", imageName: "cover1", trackName: "song1"))
+        songs.append(Song(name: "Havana", albomName: "Havana album", artistName: "Camilla Cabello", imageName: "cover2", trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida", albomName: "123 Something", artistName: "Coldplay", imageName: "cover3", trackName: "song3"))
+        songs.append(Song(name: "Background music", albomName: "123 Other", artistName: "Rnado", imageName: "cover1", trackName: "song1"))
+        songs.append(Song(name: "Havana", albomName: "Havana album", artistName: "Camilla Cabello", imageName: "cover2", trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida", albomName: "123 Something", artistName: "Coldplay", imageName: "cover3", trackName: "song3"))
+        songs.append(Song(name: "Background music", albomName: "123 Other", artistName: "Rnado", imageName: "cover1", trackName: "song1"))
+        songs.append(Song(name: "Havana", albomName: "Havana album", artistName: "Camilla Cabello", imageName: "cover2", trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida", albomName: "123 Something", artistName: "Coldplay", imageName: "cover3", trackName: "song3"))
     }
 }
--- Music/Model/Song.swift
+++ Music/Model/Song.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 struct Song {
-    
+    let name: String
+    let albomName: String
+    let artistName: String
+    let imageName: String
+    let trackName: String
 }
